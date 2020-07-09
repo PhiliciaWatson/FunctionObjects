c46f9fc0c68768324455ed50bb6a1b11d6819796
PhiliciaWatson <philiciawatson7@gmail.com>
2020-07-09 07:25:52 -0400

    Changes

---
+++ js/app.js
@@ -0,0 +1,124 @@
+//Task 2&3
+function Butter(val1,val2){
+return val1 + val2;
+}
+console.log(Butter(3,7));
+//Task 4
+function Scotch(val1,val2,val3){
+return val1 + val2 + val3;
+}
+console.log(Scotch(2,7,10));
+//Task 5
+function Pudding(val1,val2,val3,val4){
+    return val1 + val2 + val3 + val4;
+}
+console.log(Pudding(2,6,19,45))
+//Task 6
+var Desktop = {
+    "phili":"homework",
+    "pictures":"floffy dogs",
+    "Music":"Mad Hatter by Melanie Martinez",
+
+}
+//Task 7
+ var philiValue = Desktop.phili;
+ console.log(philiValue = Desktop.phili)
+
+var picturesValue = Desktop.pictures;
+console.log(picturesValue = Desktop.pictures)
+
+var MusicValue = Desktop.Music;
+console.log(MusicValue = Desktop.Music)
+//Task 8
+Desktop["phili"];
+console.log(Desktop["phili"])
+
+Desktop["pictures"];
+console.log(Desktop["pictures"])
+
+Desktop["Music"];
+console.log(Desktop["Music"])
+//Task 9
+Desktop.sing = "I can't sing";
+console.log(Desktop.sing = "I can't sing")
+
+Desktop["sing"] = "I can't sing"
+console.log(Desktop["sing"] = "I can't sing")
+//Task 10
+Desktop.phili = "no homework";
+console.log(Desktop.phili = "no homework")
+
+
+
+var Food = {
+    "soup":"chicken noodle soup",
+    "vegetables":"broccoli",
+    "fruit":"plums",
+}
+//Task 7
+var soupValue = Food.soup;
+console.log(soupValue = Food.soup)
+
+var vegetablesValue = Food.vegetables
+console.log(vegetablesValue = Food.vegetables)
+
+var fruitValue = Food.fruit
+console.log(fruitValue = Food.fruit)
+
+//Task 8
+Food["soup"];
+console.log(Food["soup"])
+
+Food["vegetables"];
+console.log(Food["vegetables"])
+
+Food["fruit"];
+console.log(Food["fruit"])
+
+//Task9
+Food.pasteries = "cake";
+console.log(Food.pasteries = "cake")
+
+Desktop["pasteries"] = "cake"
+console.log(Desktop["pasteries"] = "cake")
+
+//Task 10
+Food["fruit"] = "strawberries";
+console.log(Food["fruit"] = "strawberries")
+
+
+var Breakfast = {
+    "eggs":"omelette",
+    "waffles":"blueberry",
+    "pancakes":"plain"
+}
+//Task 7
+var BreakfastValue = Breakfast.eggs;
+console.log(BreakfastValue = Breakfast.eggs)
+
+var BreakfastValue = Breakfast.waffles;
+console.log(BreakfastValue = Breakfast.waffles)
+
+var BreakfastValue = Breakfast.pancakes;
+console.log(BreakfastValue = Breakfast.pancakes)
+
+//Task 8
+Breakfast["eggs"];
+console.log(Breakfast["eggs"])
+
+Breakfast["waffles"];
+console.log(Breakfast["waffles"])
+
+Breakfast["pancakes"];
+console.log(Breakfast["pancakes"])
+
+//Task 9
+Breakfast.oatmeal = "sweetened";
+console.log(Breakfast.oatmeal = "sweetened")
+
+Breakfast["oatmeal"] = "sweetened"
+console.log(Breakfast["oatmeal"] = "sweetened")
+
+//Task 10
+Breakfast.pancakes = "with syrup";
+console.log(Breakfast.pancakes = "with syrup")
